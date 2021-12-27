d40cc79b07a4a328ce677e5ad76d9cfade235c53
Yolanare <y09a3115@gmail.com>
2021-12-27 21:35:15 +0100

    y logo welcome landing

--- instant/app/main.js
+++ instant/app/main.js
@@ -1,10 +1,22 @@
 //# Scripts #//
 
 //- Setup -
-import './style.scss'
+import './style.scss';
+
 
 
 //- Variables -
 
 
 //- Scripts -
+function WelcomeSpawnAnimation() { // Step 1
+    console.log('spawn');
+}
+
+function WelcomeInteractionAnimation() { // Step 2
+    console.log('click');
+}
+
+window.addEventListener("DOMContentLoaded", WelcomeSpawnAnimation);
+
+window.addEventListener("click", WelcomeInteractionAnimation);
